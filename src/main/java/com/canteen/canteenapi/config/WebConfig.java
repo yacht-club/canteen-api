ff13e4e709c3c6bdd49dff5013314ac067d4c543
Artem K <artiemq@gmail.com>
2019-03-16 17:22:05 +0300

    Add cors allowed origins

--- src/main/java/com/canteen/canteenapi/config/WebConfig.java
+++ src/main/java/com/canteen/canteenapi/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.canteen.canteenapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,6 +12,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final WebProperties webProperties;
+
+    @Autowired
+    public WebConfig(WebProperties webProperties) {
+        this.webProperties = webProperties;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String[] allowedOrigins = webProperties.getAllowedOrigins().toArray(new String[0]);
+
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600L);
+
+    }
+
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
---
+++ src/main/java/com/canteen/canteenapi/config/WebProperties.java
@@ -0,0 +1,25 @@
+package com.canteen.canteenapi.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Validated
+@Configuration
+@ConfigurationProperties(prefix = "canteen.web")
+public class WebProperties {
+
+    @NotNull
+    private List<String> allowedOrigins;
+
+    public List<String> getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+}
